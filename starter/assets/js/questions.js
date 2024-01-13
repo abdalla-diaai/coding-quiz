f10462084334f16fec2e0f1888ff9b6f44bccd2e
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-01-13 12:54:09 +0000

    check answer function added.

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,60 +1,100 @@
-var questionsBank = {
-    question1: {
+var questionBank = [
+    {
         title: 'Which of the following is not a valid JavaScript loop statement?',
         choices: ['for', 'while', 'do…while', 'until'],
         answer: 4
     },
-    question2: {
+    {
         title: 'What is the purpose of the splice() method in JavaScript?',
         choices: ['To add elements to an array', 'To remove elements from an array', 'To replace elements in an array', 'All of the above'],
         answer: 4
     },
-    question3: {
+    {
+        title: 'What is the result of the following code? var x = 5; x++;',
+        choices: ['x is now 4', 'x is now 5', 'x is now 6', 'None of the above'],
+        answer: 4
+    },
+    {
+        title: 'Which of the following is not a valid JavaScript loop statement?',
+        choices: ['for', 'while', 'do…while', 'until'],
+        answer: 4
+    },
+    {
+        title: 'What is the purpose of the splice() method in JavaScript?',
+        choices: ['To add elements to an array', 'To remove elements from an array', 'To replace elements in an array', 'All of the above'],
+        answer: 4
+    },
+    {
         title: 'What is the result of the following code? var x = 5; x++;',
         choices: ['x is now 4', 'x is now 5', 'x is now 6', 'None of the above'],
         answer: 4
     }
+];
+
+var questionTitle = document.querySelector('#question-title');
+var multiChoices = document.querySelector('#choices');
+var questionsDiv = document.querySelector('#questions');
+
+;
+function changeClass(id, currentClass, newClass) {
+    document.querySelector(id).classList.remove(currentClass);
+    document.querySelector(id).classList.add(newClass);
 };
+
 
 // variable to store user answer
 var answer = 0
 
+document.querySelector('#start').addEventListener('click', function () {
+    showQuestion(questionBank[0]);
+});
+
+
+
 // function to show question and hide start
-function showQuestion() {
+function showQuestion(question) {
     document.querySelector('#start-screen').classList.add('hide');
     changeClass('#questions', 'hide', 'visible');
-    var question = questionsBank.question1;
-    console.log(JSON.stringify(question.title));
-    const qTitle = document.createElement('div');
-    qTitle.innerHTML = JSON.stringify(question.title);
-    document.querySelector('#question-title').append(qTitle);
+    var qTitle = document.createElement('div');
+    questionTitle.append(qTitle);
+    qTitle.textContent = JSON.stringify(question.title);
 
-    const choices = JSON.parse(JSON.stringify(question.choices));
+    var choices = JSON.parse(JSON.stringify(question.choices));
     answer = JSON.stringify(question.answer);
     const choicesList = document.createElement('ul');
-    for (var i = 0; i < choices.length; i++){
+    multiChoices.append(choicesList);
+    for (var i = 0; i < choices.length; i++) {
         const choice = document.createElement('ol');
-        choice.setAttribute('data-number', i+1);
-        choice.innerHTML = `${choice.dataset.number}. ${choices[i]}`;
+        choice.setAttribute('data-number', i + 1);
+        choice.textContent = `${choice.dataset.number}. ${choices[i]}`;
         choicesList.append(choice);
     };
-    document.querySelector('#choices').append(choicesList);
+    var newDiv = document.createElement('div');
+    newDiv.setAttribute("id", "correct");
+    multiChoices.append(newDiv);
+    multiChoices.addEventListener('click', function (event) {
+            var choiceClicked = event.target;
+            var choiceNumber = choiceClicked.dataset.number;
+            var newDiv = document.querySelector('#correct');
+            if (choiceNumber === answer) {
+                newDiv.innerHTML = "correct";
+            }
+            else {
+                newDiv.innerHTML = "incorrect";
+            }
+        });
 };
 
-document.querySelector('#start').addEventListener('click', showQuestion);
-
-// function to check answer
-function checkAnswer(event) {
-    var choiceClicked = event.target;
-    var choiceNumber = choiceClicked.dataset.number;
-    console.log(choiceNumber);
-    const choiceAnswer = document.createElement('span');
-    if (answer == choiceNumber){
-        choiceAnswer.innerHTML = 'Correct';
-    }
-    else {
-        choiceAnswer.innerHTML = 'Wrong';
-    }
-    document.querySelector('#questions').append(choiceAnswer);
+function runQuiz(qBank) {
+    multiChoices.addEventListener('click', function () {
+        var question = qBank[Math.floor(Math.random() * qBank.length)];
+        questionTitle.textContent = "";
+        multiChoices.textContent = "";
+        showQuestion(question);
+    });
 };
-document.querySelector('#choices').addEventListener('click', checkAnswer);
+
+
+
+
+
